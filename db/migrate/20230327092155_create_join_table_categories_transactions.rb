9f9e62376cab332525718e0a85249268694f6922
Joseph David <jodavid444dave@gmail.com>
2023-06-07 20:56:52 +0100

    Complete all requirement for the ror  capstone project

--- db/migrate/20230327092155_create_join_table_categories_transactions.rb
+++
@@ -1,7 +0,0 @@
-class CreateJoinTableCategoriesEntities < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :categories, :entities, id: false do |t|
-      t.index [:category_id, :entities_id]
-    end
-  end
-end
--- db/migrate/20230327100012_add_author_id_to_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorIdToEntities < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :entities, :author, null: false, foreign_key: { to_table: :users }
-  end
-end
