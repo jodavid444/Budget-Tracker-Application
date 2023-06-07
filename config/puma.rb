080c9c9d125269a392d7823d20f2f835ec064878
Joseph David <jodavid444dave@gmail.com>
2023-06-07 21:30:04 +0100

    Edit gemfile lock file

--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 workers ENV.fetch("WEB_CONCURRENCY") { 4 } if ENV.fetch("RAILS_ENV", "development") == "production"
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
